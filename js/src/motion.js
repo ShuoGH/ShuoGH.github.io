63ce979f821063bcc45ed5c38a8de1ef5db85aa5
Shuo <anshuobuaa@gmail.com>
2019-03-11 11:21:50 +0000

    Site updated: 2019-03-11 11:21:50

--- js/src/motion.js
+++ js/src/motion.js
@@ -1,25 +1,25 @@
 /* global NexT, CONFIG */
 
-$(document).ready(function() {
+$(document).ready(function () {
   NexT.motion = {};
 
   var sidebarToggleLines = {
     lines: [],
-    push : function(line) {
+    push: function (line) {
       this.lines.push(line);
     },
-    init: function() {
-      this.lines.forEach(function(line) {
+    init: function () {
+      this.lines.forEach(function (line) {
         line.init();
       });
     },
-    arrow: function() {
-      this.lines.forEach(function(line) {
+    arrow: function () {
+      this.lines.forEach(function (line) {
         line.arrow();
       });
     },
-    close: function() {
-      this.lines.forEach(function(line) {
+    close: function () {
+      this.lines.forEach(function (line) {
         line.close();
       });
     }
@@ -29,47 +29,47 @@ $(document).ready(function() {
     this.el = $(settings.el);
     this.status = $.extend({}, {
       init: {
-        width  : '100%',
+        width: '100%',
         opacity: 1,
-        left   : 0,
+        left: 0,
         rotateZ: 0,
-        top    : 0
+        top: 0
       }
     }, settings.status);
   }
 
-  SidebarToggleLine.prototype.init = function() {
+  SidebarToggleLine.prototype.init = function () {
     this.transform('init');
   };
-  SidebarToggleLine.prototype.arrow = function() {
+  SidebarToggleLine.prototype.arrow = function () {
     this.transform('arrow');
   };
-  SidebarToggleLine.prototype.close = function() {
+  SidebarToggleLine.prototype.close = function () {
     this.transform('close');
   };
-  SidebarToggleLine.prototype.transform = function(status) {
+  SidebarToggleLine.prototype.transform = function (status) {
     this.el.velocity('stop').velocity(this.status[status]);
   };
 
   var sidebarToggleLine1st = new SidebarToggleLine({
-    el    : '.sidebar-toggle-line-first',
+    el: '.sidebar-toggle-line-first',
     status: {
-      arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
-      close: {width: '100%', rotateZ: '-45deg', top: '5px'}
+      arrow: { width: '50%', rotateZ: '-45deg', top: '2px' },
+      close: { width: '100%', rotateZ: '-45deg', top: '5px' }
     }
   });
   var sidebarToggleLine2nd = new SidebarToggleLine({
-    el    : '.sidebar-toggle-line-middle',
+    el: '.sidebar-toggle-line-middle',
     status: {
-      arrow: {width: '90%'},
-      close: {opacity: 0}
+      arrow: { width: '90%' },
+      close: { opacity: 0 }
     }
   });
   var sidebarToggleLine3rd = new SidebarToggleLine({
-    el    : '.sidebar-toggle-line-last',
+    el: '.sidebar-toggle-line-last',
     status: {
-      arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
-      close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+      arrow: { width: '50%', rotateZ: '45deg', top: '-2px' },
+      close: { width: '100%', rotateZ: '45deg', top: '-5px' }
     }
   });
 
@@ -82,57 +82,57 @@ $(document).ready(function() {
   var xPos, yPos;
 
   var sidebarToggleMotion = {
-    toggleEl        : $('.sidebar-toggle'),
-    dimmerEl        : $('#sidebar-dimmer'),
-    sidebarEl       : $('.sidebar'),
+    toggleEl: $('.sidebar-toggle'),
+    dimmerEl: $('#sidebar-dimmer'),
+    sidebarEl: $('.sidebar'),
     isSidebarVisible: false,
-    init            : function() {
+    init: function () {
       this.toggleEl.on('click', this.clickHandler.bind(this));
       this.dimmerEl.on('click', this.clickHandler.bind(this));
       this.toggleEl.on('mouseenter', this.mouseEnterHandler.bind(this));
       this.toggleEl.on('mouseleave', this.mouseLeaveHandler.bind(this));
       this.sidebarEl.on('touchstart', this.touchstartHandler.bind(this));
       this.sidebarEl.on('touchend', this.touchendHandler.bind(this));
-      this.sidebarEl.on('touchmove', function(e) { e.preventDefault(); });
+      this.sidebarEl.on('touchmove', function (e) { e.preventDefault(); });
 
       $(document)
-        .on('sidebar.isShowing', function() {
+        .on('sidebar.isShowing', function () {
           NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
-            {paddingRight: SIDEBAR_WIDTH},
+            { paddingRight: SIDEBAR_WIDTH },
             SIDEBAR_DISPLAY_DURATION
           );
         })
-        .on('sidebar.isHiding', function() {
+        .on('sidebar.isHiding', function () {
         });
     },
-    clickHandler: function() {
+    clickHandler: function () {
       this.isSidebarVisible ? this.hideSidebar() : this.showSidebar();
       this.isSidebarVisible = !this.isSidebarVisible;
     },
-    mouseEnterHandler: function() {
+    mouseEnterHandler: function () {
       if (this.isSidebarVisible) {
         return;
       }
       sidebarToggleLines.arrow();
     },
-    mouseLeaveHandler: function() {
+    mouseLeaveHandler: function () {
       if (this.isSidebarVisible) {
         return;
       }
       sidebarToggleLines.init();
     },
-    touchstartHandler: function(e) {
+    touchstartHandler: function (e) {
       xPos = e.originalEvent.touches[0].clientX;
       yPos = e.originalEvent.touches[0].clientY;
     },
-    touchendHandler: function(e) {
+    touchendHandler: function (e) {
       var _xPos = e.originalEvent.changedTouches[0].clientX;
       var _yPos = e.originalEvent.changedTouches[0].clientY;
       if (_xPos - xPos > 30 && Math.abs(_yPos - yPos) < 20) {
         this.clickHandler();
       }
     },
-    showSidebar: function() {
+    showSidebar: function () {
       var self = this;
 
       sidebarToggleLines.close();
@@ -140,32 +140,32 @@ $(document).ready(function() {
       this.sidebarEl.velocity('stop').velocity({
         width: SIDEBAR_WIDTH
       }, {
-        display : 'block',
-        duration: SIDEBAR_DISPLAY_DURATION,
-        begin   : function() {
-          $('.sidebar .motion-element').velocity(
-            'transition.slideRightIn',
-            {
-              stagger : 50,
-              drag    : true,
-              complete: function() {
-                self.sidebarEl.trigger('sidebar.motion.complete');
+          display: 'block',
+          duration: SIDEBAR_DISPLAY_DURATION,
+          begin: function () {
+            $('.sidebar .motion-element').velocity(
+              'transition.slideRightIn',
+              {
+                stagger: 50,
+                drag: true,
+                complete: function () {
+                  self.sidebarEl.trigger('sidebar.motion.complete');
+                }
               }
-            }
-          );
-        },
-        complete: function() {
-          self.sidebarEl.addClass('sidebar-active');
-          self.sidebarEl.trigger('sidebar.didShow');
-        }
-      });
+            );
+          },
+          complete: function () {
+            self.sidebarEl.addClass('sidebar-active');
+            self.sidebarEl.trigger('sidebar.didShow');
+          }
+        });
 
       this.sidebarEl.trigger('sidebar.isShowing');
     },
-    hideSidebar: function() {
-      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
+    hideSidebar: function () {
+      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({ paddingRight: 0 });
       this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
-      this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
+      this.sidebarEl.velocity('stop').velocity({ width: 0 }, { display: 'none' });
 
       sidebarToggleLines.init();
 
@@ -185,24 +185,24 @@ $(document).ready(function() {
   sidebarToggleMotion.init();
 
   NexT.motion.integrator = {
-    queue : [],
+    queue: [],
     cursor: -1,
-    add   : function(fn) {
+    add: function (fn) {
       this.queue.push(fn);
       return this;
     },
-    next: function() {
+    next: function () {
       this.cursor++;
       var fn = this.queue[this.cursor];
       $.isFunction(fn) && fn(NexT.motion.integrator);
     },
-    bootstrap: function() {
+    bootstrap: function () {
       this.next();
     }
   };
 
   NexT.motion.middleWares = {
-    logo: function(integrator) {
+    logo: function (integrator) {
       var sequence = [];
       var $brand = $('.brand');
       var $image = $('.custom-logo-image');
@@ -213,8 +213,8 @@ $(document).ready(function() {
 
       $brand.length > 0 && sequence.push({
         e: $brand,
-        p: {opacity: 1},
-        o: {duration: 200}
+        p: { opacity: 1 },
+        o: { duration: 200 }
       });
 
       /**
@@ -224,7 +224,7 @@ $(document).ready(function() {
        */
       function hasElement($elements) {
         $elements = Array.isArray($elements) ? $elements : [$elements];
-        return $elements.every(function($element) {
+        return $elements.every(function ($element) {
           return $element.length > 0;
         });
       }
@@ -232,9 +232,10 @@ $(document).ready(function() {
       function getMistLineSettings(element, translateX) {
         return {
           e: $(element),
-          p: {translateX: translateX},
+          p: { translateX: translateX },
           o: {
-            duration     : 500,
+            duration: 500,
+            // duration: 200,
             sequenceQueue: false
           }
         };
@@ -243,29 +244,29 @@ $(document).ready(function() {
       function pushImageToSequence() {
         sequence.push({
           e: $image,
-          p: {opacity: 1, top: 0},
-          o: {duration: 200}
+          p: { opacity: 1, top: 0 },
+          o: { duration: 200 }
         });
       }
 
       NexT.utils.isMist() && hasElement([$logoLineTop, $logoLineBottom])
-      && sequence.push(
-        getMistLineSettings($logoLineTop, '100%'),
-        getMistLineSettings($logoLineBottom, '-100%')
-      );
+        && sequence.push(
+          getMistLineSettings($logoLineTop, '100%'),
+          getMistLineSettings($logoLineBottom, '-100%')
+        );
 
       NexT.utils.isMuse() && hasElement($image) && pushImageToSequence();
 
       hasElement($title) && sequence.push({
         e: $title,
-        p: {opacity: 1, top: 0},
-        o: {duration: 200}
+        p: { opacity: 1, top: 0 },
+        o: { duration: 200 }
       });
 
       hasElement($subtitle) && sequence.push({
         e: $subtitle,
-        p: {opacity: 1, top: 0},
-        o: {duration: 200}
+        p: { opacity: 1, top: 0 },
+        o: { duration: 200 }
       });
 
       (NexT.utils.isPisces() || NexT.utils.isGemini()) && hasElement($image) && pushImageToSequence();
@@ -275,7 +276,7 @@ $(document).ready(function() {
       }
 
       if (sequence.length > 0) {
-        sequence[sequence.length - 1].o.complete = function() {
+        sequence[sequence.length - 1].o.complete = function () {
           integrator.next();
         };
         /* eslint-disable */
@@ -286,22 +287,22 @@ $(document).ready(function() {
       }
     },
 
-    menu: function(integrator) {
+    menu: function (integrator) {
 
       if (CONFIG.motion.async) {
         integrator.next();
       }
 
       $('.menu-item').velocity('transition.slideDownIn', {
-        display : null,
+        display: null,
         duration: 200,
-        complete: function() {
+        complete: function () {
           integrator.next();
         }
       });
     },
 
-    postList: function(integrator) {
+    postList: function (integrator) {
 
       //var $post = $('.post');
       var $postBlock = $('.post-block, .pagination, .comments');
@@ -319,9 +320,9 @@ $(document).ready(function() {
       function postMotion() {
         var postMotionOptions = window.postMotionOptions || {
           stagger: 100,
-          drag   : true
+          drag: true
         };
-        postMotionOptions.complete = function() {
+        postMotionOptions.complete = function () {
           // After motion complete need to remove transform from sidebar to let affix work on Pisces | Gemini.
           if (CONFIG.motion.transition.sidebar && (NexT.utils.isPisces() || NexT.utils.isGemini())) {
             $sidebarAffix.css({ 'transform': 'initial' });
@@ -355,7 +356,7 @@ $(document).ready(function() {
       }
     },
 
-    sidebar: function(integrator) {
+    sidebar: function (integrator) {
       if (CONFIG.sidebar.display === 'always') {
         NexT.utils.displaySidebar();
       }
